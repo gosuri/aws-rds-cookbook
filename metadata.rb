311bb9aa1b53039d84ff566eca3193a60f374526
Greg Osuri <gosuri@gmail.com>
2013-11-27 15:53:28 -0800

    Bumped the version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'gosuri@gmail.com'
 license          'All rights reserved'
 description      'Provides libraries, resources and providers to configure and manage Amazon Relational Database Service (Amazon RDS) with the EC2 API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
