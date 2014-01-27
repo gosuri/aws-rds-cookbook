6bfb55db6ccf9a05461c67b7f1a10617f6f96938
Greg Osuri <gosuri@gmail.com>
2014-01-27 10:05:47 -0800

    Bumped the version to 1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'gosuri@gmail.com'
 license          'MIT'
 description      'Provides libraries, resources and providers to configure and manage Amazon Relational Database Service (Amazon RDS) with the EC2 API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.1'
 recipe           "aws_rds", "installs the aws-sdk gem during compile time"
 attribute        'aws_sdk_version', description: "aws-sdk gem version", type: "string"
