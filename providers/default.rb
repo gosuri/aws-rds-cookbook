132181e07c40eb68d1343bde866cd197b839190c
Florin Stan <tom@tcnp3.com>
2014-01-28 22:10:33 -0700

    fixed the vpc creation by fixing param name (db_subnet_group_name)

--- providers/default.rb
+++ providers/default.rb
@@ -8,6 +8,8 @@ action :create do
   if @current_resource.exists
     Chef::Log.info "#{@new_resource} already exists - nothing to do."
     set_node_attrs
+    Chef::Log.info "#{@new_resource} update instance values if is required"
+    update_instance
   else
     converge_by "Create #{@new_resource}" do
       Chef::Log.info "Creating #{new_resource}. This could take upto 10 minutes"
@@ -18,7 +20,8 @@ action :create do
   end
 end
 
+
 def load_current_resource
-  @current_resource = Chef::Resource::AwsRds.new(new_resource.id)
+  @current_resource = Chef::Resource::AwsRds.new(new_resource.id)  
   @current_resource.exists = instance.exists?
 end
