52f7208643b6f6e75193f72014b4aa490aa369d1
mrmmorris <mmorris@trxtraining.com>
2014-06-17 14:06:07 -0700

    Revert "add region attribute description to README and fix spacing"
    This reverts commit 598a7c21378cf63cdfaa0c6af3a1359765342655.

--- resources/default.rb
+++ resources/default.rb
@@ -7,7 +7,7 @@ attribute :aws_secret_access_key        , kind_of: String                 , requ
 attribute :allocated_storage            , kind_of: Integer                , required: true
 attribute :auto_minor_version_upgrade   , kind_of: [TrueClass,FalseClass] , default: true
 attribute :availability_zone            , kind_of: String
-attribute :region			, kind_of: String                 , default: 'us-east-1'
+attribute :region						, kind_of: String                 , default: 'us-east-1'
 attribute :backup_retention_period      , kind_of: Integer
 attribute :character_set_name           , kind_of: String
 attribute :db_instance_class            , kind_of: String                 , required: true
