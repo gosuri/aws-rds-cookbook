a8b8284c73ab61cf9942c3e7bf3f2bf1989b9df0
Tim Schroeder <timschroeder@vidder.com>
2014-09-02 17:39:16 -0700

    remove duplicate

--- resources/default.rb
+++ resources/default.rb
@@ -17,7 +17,6 @@ attribute :db_parameter_group_name      , kind_of: String
 attribute :db_subnet_group_name         , kind_of: String
 attribute :db_security_groups           , kind_of: Array
 attribute :engine                       , kind_of: String                 , required: true , default: 'postgres'
-attribute :db_subnet_group_name         , kind_of: String
 attribute :engine_version               , kind_of: String
 attribute :iops                         , kind_of: Integer
 attribute :license_model                , kind_of: String
