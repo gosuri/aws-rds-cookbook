236f9473aa4918bc125a4cb6aa1fc7ad8b81de71
Greg Osuri <gosuri@gmail.com>
2014-09-22 18:52:06 -0700

    Bumped to 1.4.1 - Fixed naming

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,12 @@
-name             'aws_rds'
+name             'aws-rds'
 maintainer       'Greg Osuri'
 maintainer_email 'gosuri@gmail.com'
 license          'MIT'
 description      'Provides libraries, resources and providers to configure and manage Amazon Relational Database Service (Amazon RDS) with the EC2 API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.4'
-recipe           "aws_rds", "installs the aws-sdk gem during compile time"
+version          '1.4.1'
+recipe           "aws-rds", "installs the aws-sdk gem during compile time"
 attribute        'aws_sdk_version', description: "aws-sdk gem version", type: "string"
 
-
 depends 'build-essential'
-
 supports 'ubuntu'
